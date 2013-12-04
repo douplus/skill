70c03d978a1ed66419c7c3ed822953e9c3597f17
dangyang <wy36101299@gmail.com>
2013-12-04 13:57:13 +0800

    Update re_showtask.php
    add ;

--- php/re_showtask.php
+++ php/re_showtask.php
@@ -4,7 +4,7 @@
 	$realltask_ary = array();
 	//取得所有任務資訊
 	$query = "select * from `1_RE_TASK` ORDER BY TIMESTAMP DESC";
-	$res = mysql_query($query) or die('error@取得所有回覆任務資訊錯誤。')
+	$res = mysql_query($query) or die('error@取得所有回覆任務資訊錯誤。');
 	$realltask_ary = '';
 	while( $a = mysql_fetch_array($res) ){
 		$realltask_ary[] = $a[TASKID].'***'.$a[USERID].'***'.$a[CONTENT].'***'.$a[TIMESTAMP];
